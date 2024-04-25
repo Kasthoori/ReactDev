e2d59e9363d1fb43a6a8216189e1df924b9b1ce2
Kasthoori <ap.kasth@gmail.com>
2024-04-25 20:35:40 +1200

    edit two files to optimize

--- pokesman/src/SearchPokemonsUnoptimized.js
+++ pokesman/src/SearchPokemonsUnoptimized.js
@@ -1,24 +1,55 @@
-import { Input } from "@material-ui/core";
-import React, { useState } from "react";
+import { CircularProgress, Input } from "@material-ui/core";
+import React, { useState, useTransition } from "react";
 import PokemonsCard from "./PokemonsCard";
+import { makeStyles } from '@material-ui/styles';
 
+
+const useStyle = makeStyles({
+   
+    text: {
+        maxWidth: "70%"
+    }
+});
 
 
 
 const SearchPokemonsUnoptimized = () => {
 
+    // const [text, setText] = useState("");
+
+    // const onChange = (e) =>{
+    //     setText(e.target.value);
+    // }
+
+    // return(
+    //     <>
+    //         <Input onChange={onChange} value={text} />
+    //         <PokemonsCard searchQuery={text} />
+    //     </>
+    // )
+
+    // writhing the code more optimized way
+
     const [text, setText] = useState("");
+    const [ searchQuery, setSearchQuery ] = useState("");
+    const [ isPending, startTransition ] = useTransition();
+    const classes = useStyle();
 
-    const onChange = (e) =>{
+    const onChange = (e) => {
         setText(e.target.value);
-    }
 
-    return(
+        startTransition(() => {
+          setSearchQuery(e.target.value);
+        })
+    };
+
+    return (
         <>
             <Input onChange={onChange} value={text} />
-            <PokemonsCard searchQuery={text} />
+            {isPending && <CircularProgress className={classes.spinner} /> }
+            <PokemonsCard searchQuery={searchQuery} />
         </>
-    )
+    );
 
 }
 
